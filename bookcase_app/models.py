c793273ef47ef1b61d6b9967b48be2ff98fdccde
amirrezanourii <amirrezanouri1@gmail.com>
2020-08-15 23:29:05 +0430

    'models2'

--- bookcase_app/models.py
+++ bookcase_app/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, help_text="Enter a book genre")
-    
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary=
+    isbn = 
+    genre = 
+
+    def __str__(self):
+        
+
+
